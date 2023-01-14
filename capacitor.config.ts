1051fbc17dc5f5072cbbcc10b58431b09e954536
wellpo7 <husleypowell7@gmail.com>
2023-01-14 12:09:45 +0100

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'smartFarm_frontEnd',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
