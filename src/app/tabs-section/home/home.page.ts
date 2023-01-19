e3a20f16dfdae232d7ccdcb75addb3eba577c0a4
wellpo7 <husleypowell7@gmail.com>
2023-01-19 21:22:43 +0100

    home.page.html statique

--- src/app/tabs-section/home/home.page.ts
+++ src/app/tabs-section/home/home.page.ts
@@ -5,45 +5,22 @@ import { from } from 'rxjs';
 
 
 
-type categorie = {
-  matricule: string,
-  name:string,
-}
+// type categorie = {
+//   matricule: string,
+//   name:string,
+// }
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
+export class HomePage {
 
-  searchTerm!: string;
-  searchName = '';
-
-  Data:any[]= []
-  categories:categorie[] = [];
+  // Data:any[]= []
+  // categories:categorie[] = [];
 
   constructor() {}
 
-  readApi(url:string) {
-    const options:HttpOptions = {
-     url
-    }
-    return from(Http.get(options))
-   }
- 
-   ngOnInit() {
-     this.readApi("http://localhost:8282/api/categorie/all").subscribe((res: { data: string; })=>{
-       this.Data = JSON.parse(res.data)
-     });
-   }
-
-  //  searchCategorie() {
-  //   const search = encodeURIComponent(this.searchName).trim();
-  //   console.log('Recherche de la catégorie ' + search);
-  //   this.readApi('http://localhost:8282/api/categorie/' +search).subscribe((data: any) =>{
-  //     console.log(data);
-  //   });
-  // }
 
 }
